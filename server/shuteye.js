1374af56f935d0c6b0c15bdacbc246eea7aa4789
Mark Stahl <mjstahl@gmail.com>
2013-06-29 17:19:19 -0400

    Corrected a listen call to the http object.

--- server/shuteye.js
+++ server/shuteye.js
@@ -2,5 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Context-Type': 'text/plain'});
 	res.end("Hello World\n");
-})
-http.listen(8001);
+}).listen(8001, '127.0.0.1');

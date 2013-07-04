fdfdae269e49c9e30cd9d7ac0499a357a06f10cd
Mark Stahl <mjstahl@gmail.com>
2013-07-04 17:01:31 -0400

    Integrated stripe processing and validation with the buy form.

--- js/forms.js
+++ js/forms.js
@@ -7,20 +7,48 @@ $('select#session-count').change(function() {
 	$('#submit-purchase').attr('value', 'Pay $' + count + ".00");
 });
 
-$('form').submit(function(e) {
+$('#payment-form').submit(function(e) {
 	e.preventDefault();
 	$('input').removeClass('invalid');
 
 	var cardType = $.payment.cardType($('#cc-num').val());
 	$('#cc-num').toggleClass('invalid', 
 		!$.payment.validateCardNumber($('#cc-num').val()));
-    $('#cc-exp').toggleClass('invalid', 
-    	!$.payment.validateCardExpiry($('#cc-exp').payment('cardExpiryVal')));
+
+	if ($.payment.validateCardExpiry($('#cc-exp').payment('cardExpiryVal'))) {
+		$('#cc-exp').toggleClass('invalid', false);
+		var exp = $('#cc-exp').payment('cardExpiryVal');
+		$('#cc-month').val(exp.month);
+		$('#cc-year').val(exp.year);
+	} else {
+		$('#cc-exp').toggleClass('invalid', true);
+	}
+
     $('#cc-cvc').toggleClass('invalid', 
     	!$.payment.validateCardCVC($('#cc-cvc').val(), cardType));
+
+    var $form = $(this);
+    $('#submit-purchase').prop('disabled', true);
+    Stripe.createToken($form, formRespHandler);
+    return false;
 });
 
-function togglePassword() {
+var formRespHandler = function(status, resp) {
+	var $form = $('#payment-form');
+	if (resp.error) {
+		$form.find('#payment-errors')
+			 .attr('class', 'span6 alert alert-error')
+			 .text(resp.error.message);
+
+		$('#submit-purchase').prop('disabled', false);
+	} else {
+		var token = resp.id;
+		$form.append($('<input type="hidden" name="purchaseToken" />').val(token));
+		$form.get(0).submit();
+	}
+}
+
+var togglePassword = function () {
 	var show = $('i#show-password');
 	if (show.length > 0) {
 		show.attr('id', 'hide-password').attr('class', 'icon-eye-open icon-3x');

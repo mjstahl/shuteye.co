dc0310c099e35ec21a9f9278e78fb67289817a8b
Mark Stahl <mjstahl@gmail.com>
2013-07-09 12:34:17 -0400

    Moved the toggle password function to the forms.js. Changed the link in the pwd.html file.

--- js/forms.js
+++ js/forms.js
@@ -43,3 +43,14 @@ var formRespHandler = function(status, resp) {
 		$form.get(0).submit();
 	}
 }
+
+var togglePassword = function () {
+var show = $('i#show-password');
+	if (show.length > 0) {
+		show.attr('id', 'hide-password').attr('class', 'icon-eye-open icon-3x');
+		$('input#password').attr('type', 'text');
+	} else {
+		$('i#hide-password').attr('id', 'show-password').attr('class', 'icon-eye-close icon-3x');
+		$('input#password').attr('type', 'password');
+	}
+}

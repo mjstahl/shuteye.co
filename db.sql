e83cd369b86c7025fb2ae0ad32517db1155ab1f9
Mark Stahl <mjstahl@gmail.com>
2013-09-11 16:02:24 -0400

    Moved session_id to be the last field in the shuteye table.

--- db.sql
+++ db.sql
@@ -3,8 +3,8 @@ BEGIN TRANSACTION;
 CREATE TABLE shuteye (
 host_id VARCHAR(40),
 attendee_id VARCHAR(40),
-session_id VARCHAR(40),
 password TEXT,
 sessions_left TINYINT,
+session_id VARCHAR(40) NULL,
 PRIMARY KEY (host_id));
 COMMIT;
